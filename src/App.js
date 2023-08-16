b2ceefdd2f5ab393c4eff3090c98dcb7a4781d25
Babatunde <68726835+tundeloper@users.noreply.github.com>
2023-08-16 15:35:28 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Auth from "./components/auth";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './hearder.module.css'
+export default function Header() {
+  return (
+      <div className={style.container}>
+          <div>nairafill</div>
+          <div className={style.buttonContainer}>
+              <div >
+                  <button className={style.button}>Register</button>
+              </div>
+              <div>
+                  <button className={style.button}>login</button>
+              </div>
+          </div>
+    </div>
+  )
+}
